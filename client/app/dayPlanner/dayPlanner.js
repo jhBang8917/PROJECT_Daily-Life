123f976b310bd6d97c463dffaa6d25f430adb03d
Dennis Bang <jhbang8917@gmail.com>
2015-07-19 14:00:50 +0900

    create day planner page

--- client/app/dayPlanner/dayPlanner.js
+++ client/app/dayPlanner/dayPlanner.js
@@ -1,11 +1,22 @@
 'use strict';
 
 angular.module('dailyLifeApp')
-  .config(function ($stateProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('dayPlanner', {
         url: '/dayPlanner',
-        templateUrl: 'app/dayPlanner/dayPlanner.html',
+        templateUrl: 'app/dayPlanner/dayPlanner-form.html',
         controller: 'DayPlannerCtrl'
+      })
+      .state('dayPlanner.first', { //   dayPlanner/dayPlannerFirst
+        url: '/dayPlannerFirst',
+        templateUrl: 'app/dayPlanner/dayPlanner-first.html'
+      })
+      .state('dayPlanner.second', {
+        url: '/dayPlannerSecond',
+        templateUrl: 'app/dayPlanner/dayPlanner-second.html'
       });
+
+    $urlRouterProvider.otherwise('/dayPlanner/dayPlannerFirst');
+
   });
